9c4b243ad352d8ef0b1e3771ecdcdd78c5440ba8
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-05-24 01:48:37 +0300

    Add files via upload

---
+++ 06.Cake/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _06.Cake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int cakeSize = width * length;
+            int totalPiecesTaken = 0;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "STOP")
+                {
+                    int piecesLeft = cakeSize - totalPiecesTaken;
+                    if (piecesLeft > 0)
+                    {
+                        Console.WriteLine($"{piecesLeft} pieces are left.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"No more cake left! You need {Math.Abs(piecesLeft)} pieces more.");
+                    }
+                    break;
+                }
+                int piecesTaken = int.Parse(input);
+                totalPiecesTaken += piecesTaken;
+                if (totalPiecesTaken >= cakeSize)
+                {
+                    Console.WriteLine($"No more cake left! You need " +
+                                      $"{Math.Abs(totalPiecesTaken - cakeSize)} pieces more.");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ 07.Moving/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _07.Moving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            int freeSpace = width * length * height;
+            string input = Console.ReadLine();
+            while (input != "Done")
+            {
+                int boxes = int.Parse(input);
+                freeSpace -= boxes;
+                if (freeSpace < 0)
+                {
+                    Console.WriteLine($"No more free space! You need {Math.Abs(freeSpace)} Cubic meters more.");
+                    return;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{freeSpace} Cubic meters left.");
+        }
+    }
+}
